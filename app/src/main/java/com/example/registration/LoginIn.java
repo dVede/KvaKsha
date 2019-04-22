e55a9dea346c375358c1118921b36a748db98bd2
dVede <dvede123@yandex.ru>
2019-04-22 21:39:44 +0300

    implements View.OnClickListener user image add to Database

--- app/src/main/java/com/example/registration/LoginIn.java
+++ app/src/main/java/com/example/registration/LoginIn.java
@@ -16,50 +16,46 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginIn extends AppCompatActivity {
+public class LoginIn extends AppCompatActivity implements View.OnClickListener {
+
+    EditText email = findViewById(R.id.email_edittext_login);
+    EditText password = findViewById(R.id.password_edittext_login);
+
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.login_button_login) {
+            createAccount(email.getText().toString(), password.getText().toString());
+        } else if (i == R.id.login_registration) {
+            finish();
+        }}
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Button button = findViewById(R.id.login_button_login);
-        TextView register = findViewById(R.id.login_registration);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText email = findViewById(R.id.email_edittext_login);
-                EditText password = findViewById(R.id.password_edittext_login);
-
-                String emailFind = email.getText().toString();
-                String passwordFind = password.getText().toString();
-
-                final FirebaseAuth mAuth;
-                mAuth = FirebaseAuth.getInstance();
-                mAuth.signInWithEmailAndPassword(emailFind, passwordFind)
-                        .addOnCompleteListener(LoginIn.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d("signInWithEmailSuccess", "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("signInWithEmailFail", "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(LoginIn.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
+
+    public void createAccount(String email, String password){
+            final FirebaseAuth mAuth;
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginIn.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("signInWithEmailSuccess", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("signInWithEmailFail", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginIn.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+    }
+
 }
+
--- app/src/main/java/com/example/registration/Register.java
+++ app/src/main/java/com/example/registration/Register.java
@@ -5,98 +5,102 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
 
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener{
 
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView already = findViewById(R.id.already_registartion);
-        Button button = findViewById(R.id.register_button_register);
-        Button photo = findViewById(R.id.selectphoto_button_register);
-
-        photo.setOnClickListener(new View.OnClickListener() {
-
-
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent,0);
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText username = findViewById(R.id.username_edittext_register);
-                EditText email = findViewById(R.id.email_edittext_register);
-                EditText password = findViewById(R.id.password_eddittext_register);
-
-                String usernameFind = username.getText().toString();
-                String emailFind = email.getText().toString();
-                String passwordFind = password.getText().toString();
-
-                Log.d("Register", "Email is: " + emailFind);
-                Log.d("Register", "Password is: " + passwordFind);
-                Log.d("Register", "Username is: " + usernameFind);
-
-                final FirebaseAuth mAuth;
-                mAuth = FirebaseAuth.getInstance();
-                    mAuth.createUserWithEmailAndPassword(emailFind, passwordFind)
-                            .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        Log.d("emailCreateSuccess", "createUserWithEmail:success");
-                                        FirebaseUser user = mAuth.getCurrentUser();
-                                    } else {
-                                        // If sign in fails, display a message to the user.
-                                        Log.w("emailCreateFail", "createUserWithEmail:failure", task.getException());
-                                        Toast.makeText(Register.this, "Authentication failed.",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-            }
-        });
-
-        already.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("Register", "Try to show login activity");
-                Intent intent = new Intent(Register.this, LoginIn.class);
-                startActivity(intent);
-            }
-        });
-    }
+    EditText password = findViewById(R.id.password_eddittext_register);
+    EditText username = findViewById(R.id.username_edittext_register);
+    EditText email = findViewById(R.id.email_edittext_register);
 
     Uri selectedPhoto = null;
+
+    public void photo(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    public void already(){
+        Log.d("Register", "Try to show login activity");
+        Intent intent = new Intent(Register.this, LoginIn.class);
+        startActivity(intent);
+    }
+
+    public void registerAccount(String email, String password){
+        Log.d("Register", "Email is: " + email);
+        Log.d("Register", "Password is: " + password);
+        Log.d("Register", "Username is: " + username);
+
+        final FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("emailCreateSuccess", "createUserWithEmail:success");
+                            uploadUserImageToFirebase();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("emailCreateFail", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.register_button_register) {
+            registerAccount(email.getText().toString(), password.getText().toString());
+        } else if (i == R.id.already_registartion) {
+            already();
+        }
+        else if (i == R.id.selectphoto_button_register) {
+            photo();
+        }}
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Register.super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
            selectedPhoto = data.getData();
@@ -114,4 +118,42 @@ public class Register extends AppCompatActivity {
         }
     }
 
+    public void uploadUserImageToFirebase(){
+        String filename = UUID.randomUUID().toString();
+        final StorageReference ref = FirebaseStorage.getInstance().getReference("/images/" + filename);
+        StorageTask<UploadTask.TaskSnapshot> registerActivity = ref.putFile(selectedPhoto)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot authResult) {
+                Log.d("RegisterActivity", "Successfully upload image:" +
+                        Objects.requireNonNull(authResult.getMetadata()).getPath());
+                ref.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>(){
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        Log.d("RegisterActivity", "File location " + task);
+                        saveUserInDatabase(task.toString());
+                    }
+                });
+            }
+                });
+    }
+    public void saveUserInDatabase(String profileImageUrl){
+        String uid = FirebaseAuth.getInstance().getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/users/" + uid);
+        EditText username = findViewById(R.id.username_edittext_register);
+
+        Object user = new User(uid, username.getText().toString(), profileImageUrl);
+        ref.setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("RegisterActivity", "ADDED");
+            }
+        });
+    }
+}
+class User {
+    private String uid, username, profileImageUrl;
+    User(String uid, String toString, String profileImageUrl) {
+    }
 }
