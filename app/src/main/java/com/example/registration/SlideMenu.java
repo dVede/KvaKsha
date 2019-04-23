500e33226c54be150f25ead45beb16f651aa55ec
dVede <dvede123@yandex.ru>
2019-04-23 16:00:58 +0300

    Slide menu fullCreated

--- app/src/main/java/com/example/registration/SlideMenu.java
+++ app/src/main/java/com/example/registration/SlideMenu.java
@@ -1,13 +1,36 @@
 package com.example.registration;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class SlideMenu extends AppCompatActivity {
 
+    private ActionBarDrawerToggle mToggle;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide_menu);
+        DrawerLayout mDrawerLayout = findViewById(R.id.slide_menu);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
