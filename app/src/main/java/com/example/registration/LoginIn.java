3b7af23fd2b6bfb12d3dc853dd855c58e19c117a
dVede <dvede123@yandex.ru>
2019-04-23 13:19:24 +0300

    Save user information in Firebase Database

--- app/src/main/java/com/example/registration/LoginIn.java
+++ app/src/main/java/com/example/registration/LoginIn.java
@@ -17,6 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginIn extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/registration/Register.java
+++ app/src/main/java/com/example/registration/Register.java
@@ -17,55 +17,60 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
-
+import java.util.UUID;
 
 public class Register extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText username = findViewById(R.id.username_edittext_register);
+        EditText email = findViewById(R.id.email_edittext_register);
+        EditText password = findViewById(R.id.password_eddittext_register);
+
+        final String usernameFind = username.getText().toString();
+        final String emailFind = email.getText().toString();
+        final String passwordFind = password.getText().toString();
+
         TextView already = findViewById(R.id.already_registartion);
-        final Button button = findViewById(R.id.register_button_register);
+        Button button = findViewById(R.id.register_button_register);
         Button photo = findViewById(R.id.selectphoto_button_register);
 
         photo.setOnClickListener(new View.OnClickListener() {
 
-
             public void onClick(View v) {
+                Log.d("RegisterActivity", "Try to show photo selector");
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(intent,0);
+                startActivityForResult(intent, 0);
             }
         });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText username = findViewById(R.id.username_edittext_register);
-                EditText email = findViewById(R.id.email_edittext_register);
-                EditText password = findViewById(R.id.password_eddittext_register);
-
-                String usernameFind = username.getText().toString();
-                String emailFind = email.getText().toString();
-                String passwordFind = password.getText().toString();
-
                 if (emailFind.isEmpty() || passwordFind.isEmpty()) {
                     Toast.makeText(Register.this , "Pleas enter text in email/pw", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                Log.d("Register", "Email is: " + emailFind);
-                Log.d("Register", "Password is: " + passwordFind);
-                Log.d("Register", "Username is: " + usernameFind);
+                Log.d("RegisterActivity", "Try to create user with email: " + emailFind);
+                Log.d("RegisterActivity", "Try to create user with password: " + passwordFind);
+                Log.d("RegisterActivity", "Try to create user with username:" + usernameFind);
 
                 final FirebaseAuth mAuth;
                 mAuth = FirebaseAuth.getInstance();
@@ -73,11 +78,11 @@ public class Register extends AppCompatActivity {
                         .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d("emailCreateSuccess", "createUserWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
+                                    uploadImageToStorage();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w("emailCreateFail", "createUserWithEmail:failure", task.getException());
@@ -92,7 +97,7 @@ public class Register extends AppCompatActivity {
         already.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Register", "Try to show login activity");
+                Log.d("RegisterActivity", "Try to show login activity");
                 Intent intent = new Intent(Register.this, LoginIn.class);
                 startActivity(intent);
             }
@@ -104,6 +109,7 @@ public class Register extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Register.super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            Log.d("RegisterActivity", "Photo selected");
             selectedPhoto = data.getData();
             Bitmap bitmap = null;
             try {
@@ -118,4 +124,56 @@ public class Register extends AppCompatActivity {
         }
     }
 
+    public  void uploadImageToStorage(){
+        if (selectedPhoto == null)
+            return;
+        String filename = UUID.randomUUID().toString();
+        final StorageReference ref = FirebaseStorage.getInstance().getReference("/image/" + filename);
+        ref.putFile(selectedPhoto).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+        @Override
+        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+            Log.d("RegisterActivity", "Successfully uploaded image:" + taskSnapshot.getMetadata().getPath());
+            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Log.d("RegisterActivity", "FileLocation " + uri);
+                    saveUserInDatabase(uri.toString());
+                }
+            });
+        }
+    }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("RegisterActivity", "Failed to upload image to storage");
+            }
+        });
+    }
+
+    private void saveUserInDatabase(String profileImageUrl){
+        String uid = FirebaseAuth.getInstance().getUid();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        EditText username = findViewById(R.id.username_edittext_register);
+        User user = new User(uid, username.getText().toString(), profileImageUrl);
+        ref.child(user.uid).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("RegisterActivity", "Saved in Database");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("RegisterActivity", "Failed to save in Database");
+            }
+        });
+    }
+}
+class User{
+    String uid, username, profileImageUrl;
+
+    User(String uid, String username, String profileImageUrl) {
+        this.username = username;
+        this.uid = uid;
+        this.profileImageUrl = profileImageUrl;
+    }
 }
