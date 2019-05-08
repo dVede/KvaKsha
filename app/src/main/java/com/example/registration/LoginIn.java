605d8b9529a8b51501ed71f8a08aa48281ee678e
dVede <dvede123@yandex.ru>
2019-05-08 23:18:09 +0300

    onClick, setOnClickListener fix in Registration/loginIn

--- app/src/main/java/com/example/registration/LoginIn.java
+++ app/src/main/java/com/example/registration/LoginIn.java
@@ -2,12 +2,11 @@ package com.example.registration;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,60 +15,61 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginIn extends AppCompatActivity {
+public class LoginIn extends AppCompatActivity implements View.OnClickListener{
+
+    EditText email = findViewById(R.id.email_edittext_login);
+    EditText password = findViewById(R.id.password_edittext_login);
+
+    String emailFound = email.getText().toString();
+    String passwordFound = password.getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button = findViewById(R.id.login_button_login);
-        TextView register = findViewById(R.id.login_registration);
+        findViewById(R.id.login_button_login).setOnClickListener(this);
+        findViewById(R.id.login_registration).setOnClickListener(this);
+    }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText email = findViewById(R.id.email_edittext_login);
-                EditText password = findViewById(R.id.password_edittext_login);
+    public void signIn(String email, String password) {
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(LoginIn.this , "Pleas enter text in email/pw", Toast.LENGTH_LONG).show();
+            //TODO: move toast string to strings.xml
+            return;
+        }
 
-                String emailFind = email.getText().toString();
-                String passwordFind = password.getText().toString();
+        final FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("signInWithEmailSuccess", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginIn.this, SlideMenu.class);
+                            startActivity(intent);
+                            finish();
+                            //TODO: showChat() for chowing main chat layout
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("signInWithEmailFail", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginIn.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
 
-                if (emailFind.isEmpty() || passwordFind.isEmpty()) {
-                    Toast.makeText(LoginIn.this , "Pleas enter text in email/pw", Toast.LENGTH_SHORT).show();
-                    //TODO: move toast string to strings.xml
-                    return;
-                }
-
-                final FirebaseAuth mAuth;
-                mAuth = FirebaseAuth.getInstance();
-                mAuth.signInWithEmailAndPassword(emailFind, passwordFind)
-                        .addOnCompleteListener(LoginIn.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d("signInWithEmailSuccess", "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    Intent intent = new Intent(LoginIn.this, SlideMenu.class);
-                                    startActivity(intent);
-                                    finish();
-                                    //TODO: showChat() for chowing main chat layout
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("signInWithEmailFail", "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(LoginIn.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.login_registration) {
+            finish();
+        } else if (i == R.id.login_button_login) {
+            signIn(emailFound, passwordFound);
+        }
     }
 }
