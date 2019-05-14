c1de1848183b3f78256c6e04295242db35e9a6d4
dVede <dvede123@yandex.ru>
2019-05-14 19:30:44 +0300

    NavigationViewListener created

--- app/src/main/java/com/example/registration/SlideMenu.java
+++ app/src/main/java/com/example/registration/SlideMenu.java
@@ -6,12 +6,14 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +29,8 @@ public class SlideMenu extends AppCompatActivity implements NavigationView.OnNav
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) { //TODO: replace placeholders in header.xml
+        ListView mDrawerListView;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide_menu);
         DrawerLayout mDrawerLayout = findViewById(R.id.slide_menu);
@@ -34,10 +38,46 @@ public class SlideMenu extends AppCompatActivity implements NavigationView.OnNav
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+        int id = menuItem.getItemId();
+        switch (id){
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(SlideMenu.this, LoginIn.class);
+                startActivity(intent);
+                break;
+            case R.id.private_messages:
+                break;
+            case R.id.chatroom_messages:
+                break;
+            case R.id.create_chatroom:
+                break;
+            default:
+        }
+        DrawerLayout drawer = findViewById(R.id.slide_menu);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.slide_menu);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
