2b3f3b3b2d038c878acbe448a9f5f670bc0faea4
NevostruevaYana <43248752+NevostruevaYana@users.noreply.github.com>
2019-05-16 18:43:18 +0300

    pull request #5 (#5)
    * commit
    
    * commit

--- app/src/main/java/com/example/registration/EnterChatroomMessage.java
+++ app/src/main/java/com/example/registration/EnterChatroomMessage.java
@@ -1,25 +1,31 @@
 package com.example.registration;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EnterChatroomMessage extends AppCompatActivity {
 
-    DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +43,7 @@ public class EnterChatroomMessage extends AppCompatActivity {
                 final String chatroomNameFind = createChatroomName.getText().toString();
                 final String passwordChatroomFind = createPasswordChatroom.getText().toString();
 
-                final List<String> chatroomList = new ArrayList<>();
-                reference.child("/chatrooms/").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot child: dataSnapshot.getChildren()){
-                            chatroomList.add(child.toString());
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
+                ref = FirebaseDatabase.getInstance().getReference();
 
                 if (chatroomNameFind.contains("@")){
                     Toast.makeText(EnterChatroomMessage.this , "Don't use the @ symbol", Toast.LENGTH_SHORT).show();
@@ -65,8 +58,43 @@ public class EnterChatroomMessage extends AppCompatActivity {
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        Query usernameQuery = FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("chatroomName").equalTo(chatroomNameFind);
+                        usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.getChildrenCount() == 0){
+                                    Toast.makeText(EnterChatroomMessage.this, "Such chatroom do not exist", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    ref.child("/chatrooms/" + chatroomNameFind).addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            Chatroom chatroom = dataSnapshot.getValue(Chatroom.class);
+                                            String pw = chatroom.getPw();
+                                            if (pw.equals(passwordChatroomFind)){
+                                                Intent intent = new Intent(EnterChatroomMessage.this, Message.class);
+                                                intent.putExtra("chatroomName", chatroomNameFind);
+                                                startActivity(intent);
+                                            } else {
+                                                Toast.makeText(EnterChatroomMessage.this, "asdfghyjukil", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
 
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }
+
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
                     }
+
+
                 });
             }
         });
--- app/src/main/java/com/example/registration/EnterPrivateMessage.java
+++ app/src/main/java/com/example/registration/EnterPrivateMessage.java
@@ -1,18 +1,29 @@
 package com.example.registration;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -22,8 +33,8 @@ import java.util.TreeMap;
 
 public class EnterPrivateMessage extends AppCompatActivity {
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference reference = database.getReference();
+    FirebaseUser firebaseUser;
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +50,15 @@ public class EnterPrivateMessage extends AppCompatActivity {
 
                 final String userFind = pmUser.getText().toString();
 
+
                 if (userFind.isEmpty()) {
                     Toast.makeText(EnterPrivateMessage.this , "Please enter text in Username", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                ref = FirebaseDatabase.getInstance().getReference();
+
 
                 final Map<String ,String> uidAndUsernameMap = new TreeMap<>();
                 final String[] currenUsername = new String[1];
@@ -77,9 +93,43 @@ public class EnterPrivateMessage extends AppCompatActivity {
                     }
                 });
 
+
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+
+                        ref.child("/users/" + firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                User user = dataSnapshot.getValue(User.class);
+                                final String username = user.getUsername();
+                                Query usernameQuery = FirebaseDatabase.getInstance().getReference().child("users").orderByChild("username").equalTo(userFind);
+                                usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.getChildrenCount() > 0){
+                                            String chatroomName = "@" + username + "@" + userFind;
+                                            ref.child("/chatrooms/" +chatroomName).setValue(new Chatroom("@" + username + "@" + userFind));
+                                            Intent intent = new Intent(EnterPrivateMessage.this, Message.class);
+                                            intent.putExtra("chatroomName", chatroomName);
+                                            startActivity(intent);
+                                        } else {
+                                            Toast.makeText(EnterPrivateMessage.this, "Such user does not exist", Toast.LENGTH_SHORT).show();
+                                        }
+
+                                    }
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
                         if(chatroomList.contains("@" + currenUsername[0] + "@" + userFind) ||
                                 chatroomList.contains("@" + userFind + "@" + currenUsername[0])){
                             Toast.makeText(EnterPrivateMessage.this, "++++++", Toast.LENGTH_SHORT).show();
@@ -91,6 +141,7 @@ public class EnterPrivateMessage extends AppCompatActivity {
                                 Toast.makeText(EnterPrivateMessage.this, "Such user does not exist", Toast.LENGTH_SHORT).show();
                             }
                         }
+
                     }
                 });
             }
