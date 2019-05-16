2f5e11fd377fef0518fc7e2c6dce84004aefb394
NevostruevaYana <43248752+NevostruevaYana@users.noreply.github.com>
2019-05-16 23:05:59 +0300

    pull request #6 (#8)
    * commit
    
    * commit
    
    * commit
    
    * commit

--- app/src/main/java/com/example/registration/EnterPrivateMessage.java
+++ app/src/main/java/com/example/registration/EnterPrivateMessage.java
@@ -23,13 +23,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class EnterPrivateMessage extends AppCompatActivity {
 
@@ -59,45 +52,9 @@ public class EnterPrivateMessage extends AppCompatActivity {
                 firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
                 ref = FirebaseDatabase.getInstance().getReference();
 
-
-                final Map<String ,String> uidAndUsernameMap = new TreeMap<>();
-                final String[] currenUsername = new String[1];
-                final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                reference.child("/users/").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot child: dataSnapshot.getChildren()){
-                                uidAndUsernameMap.put(child.getKey(), child.child("/username/").getValue().toString());
-                                if(child.getKey().equals(uid)){
-                                    currenUsername[0] = child.child("/username/").getValue().toString();
-                                }
-                            }
-                        }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                    }
-                });
-
-                final List<String> chatroomList = new ArrayList<>();
-                reference.child("/chatrooms/").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot child: dataSnapshot.getChildren()){
-                            chatroomList.add(child.getKey());
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                    }
-                });
-
-
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
                         ref.child("/users/" + firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -127,21 +84,6 @@ public class EnterPrivateMessage extends AppCompatActivity {
 
                             @Override
                             public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                            }
-                        });
-                        if(chatroomList.contains("@" + currenUsername[0] + "@" + userFind) ||
-                                chatroomList.contains("@" + userFind + "@" + currenUsername[0])){
-                            Toast.makeText(EnterPrivateMessage.this, "++++++", Toast.LENGTH_SHORT).show();
-                        } else {
-                            if (uidAndUsernameMap.values().contains(userFind)) {
-                                reference.child("/chatrooms/" + "@" + currenUsername[0] + "@" + userFind + "/chatroomPW/").setValue("0000");
-
-                            } else {
-                                Toast.makeText(EnterPrivateMessage.this, "Such user does not exist", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
                     }
                 });
             }
